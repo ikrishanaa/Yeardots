d9f3aaa6d445f103840160618f79365082f9ab56
ikrishanaa <krishanaindia773@gmail.com>
2026-08-07 02:34:10 +0530

    chore: release v2.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.krishana.onedot"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "2.0.3"
+        versionCode = 11
+        versionName = "2.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
